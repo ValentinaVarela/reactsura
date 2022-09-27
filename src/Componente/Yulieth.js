5da7892a590c788ea1447f0fa3d26da970ff3335
407 <407@academicos.cesde.edu.co>
2022-09-27 17:42:21 -0500

    se crea app react

---
+++ src/Componente/Yulieth.js
@@ -0,0 +1,12 @@
+export function Yulieth(){
+
+    let nombre="Yulieth"
+    //interpolar=controlar la GUI desde JS
+
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre}</h1>
+            <hr/>
+        </>
+    )
+}
---
+++ src/Sinopsis/Sinopsis.js
@@ -0,0 +1,11 @@
+export function Sinopsis(){
+    return(
+        <>
+            <p>
+                <h2>
+                El maquillaje es la práctica de decorar la piel y otras partes visibles del cuerpo para resaltarlas o mejorar su aspecto. Por extensión, el término designa también los cosméticos que se emplean, tales como los lápices de labios y sombras de ojos.
+                </h2>
+            </p>
+        </>
+    )
+}
